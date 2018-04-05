7a372cadf46e0d7a40086c3d37ad40cebd05568a
danielchisica <daniel.chisica@hotmail.com>
2018-04-05 09:13:45 -0500

    Done

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+/**
+* This function refreshes date and time at the html, in an one second intervals
+*/
 (function () {
 
-        var refreshTime = function () {
-            var date = new Date(),
+    var refreshTime = function () {
+        var date = new Date(),
             hours = date.getHours(),
             meridiem,
             minutes = date.getMinutes(),
             seconds = date.getSeconds(),
-            weekDay= date.getDay(),
+            weekDay = date.getDay(),
             day = date.getDate(),
             month = date.getMonth(),
             year = date.getFullYear();
 
-            var pHours = document.getElementById('hours'),
+        var pHours = document.getElementById('hours'),
             pMeridiem = document.getElementById('meridiem'),
             pMinutes = document.getElementById('minutes'),
             pSeconds = document.getElementById('seconds'),
@@ -20,39 +23,43 @@
             pMonth = document.getElementById('month'),
             pYear = document.getElementById('year');
 
-            var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-            pWeekDay.textContent = week[weekDay];
+        pWeekDay.textContent = week[weekDay];
 
-            pDay.textContent = day;
+        pDay.textContent = day;
 
-            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-            pMonth.textContent = months[month];
+        pMonth.textContent = months[month];
 
-            pYear.textContent = year;
+        pYear.textContent = year;
 
-            if (hours >= 12) {
-                hours = hours - 12;
-                meridiem = 'PM';
-            } else {
-                meridiem = 'AM';
-            }
+        if (hours >= 12) {
+            hours = hours - 12;
+            meridiem = '  PM';
+        } else {
+            meridiem = '  AM';
+        }
 
-            if (hours == 0) {
-                hours = 12;
-            };
-
-            pHours.textContent = hours;
-            pMeridiem.textContent = meridiem;
-
-            if(minutes < 10){ minutes = '0' + minutes};
-            if(seconds < 10){seconds = '0' + seconds};
-            pMinutes.textContent = minutes;
-            pSeconds.textContent = seconds;
-
-
+        if (hours == 0) {
+            hours = 12;
         };
+
+        pHours.textContent = hours;
+        pMeridiem.textContent = meridiem;
+
+        if (minutes < 10) {
+            minutes = '0' + minutes
+        };
+        if (seconds < 10) {
+            seconds = '0' + seconds
+        };
+        pMinutes.textContent = minutes;
+        pSeconds.textContent = seconds;
+
+
+    };
     refreshTime;
     var interval = setInterval(refreshTime, 1000);
 }())
