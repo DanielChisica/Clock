426e394a8e4b42ca26426573c60c37a216e8704e
danielchisica <daniel.chisica@hotmail.com>
2018-04-05 00:58:01 -0500

    Implement Styles

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
-var months =['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+(function () {
+
+        var refreshTime = function () {
+            var date = new Date(),
+            hours = date.getHours(),
+            meridiem,
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds(),
+            weekDay= date.getDay(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            year = date.getFullYear();
+
+            var pHours = document.getElementById('hours'),
+            pMeridiem = document.getElementById('meridiem'),
+            pMinutes = document.getElementById('minutes'),
+            pSeconds = document.getElementById('seconds'),
+            pWeekDay = document.getElementById('weekDay'),
+            pDay = document.getElementById('day'),
+            pMonth = document.getElementById('month'),
+            pYear = document.getElementById('year');
+
+            var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+            pWeekDay.textContent = week[weekDay];
+
+            pDay.textContent = day;
+
+            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            pMonth.textContent = months[month];
+
+            pYear.textContent = year;
+
+            if (hours >= 12) {
+                hours = hours - 12;
+                meridiem = 'PM';
+            } else {
+                meridiem = 'AM';
+            }
+
+            if (hours == 0) {
+                hours = 12;
+            };
+
+            pHours.textContent = hours;
+            pMeridiem.textContent = meridiem;
+
+            if(minutes < 10){ minutes = '0' + minutes};
+            if(seconds < 10){seconds = '0' + seconds};
+            pMinutes.textContent = minutes;
+            pSeconds.textContent = seconds;
 
 
-var refresh= function(){
-    var mydate= new Date();
-
-document.getElementById("Day").innerHTML =days[mydate.getDay()-1] + ',' +mydate.getUTCDate();
-document.getElementById("Month").innerHTML =months[mydate.getMonth()-1];
-document.getElementById("Year").innerHTML =mydate.getFullYear();
-
-document.getElementById("Hour").innerHTML =mydate.getHours();
-document.getElementById("Minutes").innerHTML =mydate.getMinutes();
-document.getElementById("Seconds").innerHTML =mydate.getSeconds();
-
-}
-
-setInterval(refresh,1000);
-
-
-
+        };
+    refreshTime;
+    var interval = setInterval(refreshTime, 1000);
+}())
